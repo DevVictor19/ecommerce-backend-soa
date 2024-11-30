8cbf8766d6d32a0bfaaefe21855e795142845408
DevVictor19 <antoniovictor12@live.com>
2024-11-30 14:59:57 -0300

    feat(users): import infrastructure modules

--- users/src/app.module.ts
+++ users/src/app.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
 
-@Module({})
+import { DatabaseModule } from './database/database.module';
+import { EnvConfigModule } from './env-config/env-config.module';
+
+@Module({
+  imports: [DatabaseModule, EnvConfigModule],
+})
 export class AppModule {}
