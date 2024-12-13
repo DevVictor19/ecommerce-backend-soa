0e2ed68184f9c67307785c32149acad7df1e7983
DevVictor19 <antoniovictor12@live.com>
2024-12-13 14:08:45 -0300

    feat(payments): initial commit

---
+++ payments/src/infra/database/database.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { EnvConfigService } from '../env-config/services/env-config-service.interface';
+
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: (evnConfigService: EnvConfigService) => ({
+        uri: evnConfigService.getDbUri(),
+        dbName: evnConfigService.getDbName(),
+        user: evnConfigService.getDbUser(),
+        pass: evnConfigService.getDbPassword(),
+        autoIndex: true,
+      }),
+      inject: [EnvConfigService],
+    }),
+  ],
+})
+export class DatabaseModule {}
---
+++ payments/src/infra/env-config/services/env-config-service.interface.ts
@@ -0,0 +1,8 @@
+export abstract class EnvConfigService {
+  abstract getDbUri(): string;
+  abstract getDbName(): string;
+  abstract getDbUser(): string;
+  abstract getDbPassword(): string;
+  abstract getDbPort(): number;
+  abstract getServerPort(): number;
+}
