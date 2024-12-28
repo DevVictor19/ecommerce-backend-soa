d9a7a74dbbaeb26b0adf96576c687917ff8fb77f
DevVictor19 <antoniovictor12@live.com>
2024-12-28 15:59:45 -0300

    refactor(payments): remove unused logger

--- payments/src/core/products/services/implementations/product.service.ts
+++ payments/src/core/products/services/implementations/product.service.ts
@@ -1,12 +1,10 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 
 import { CartProductDto, ProductService } from '../product-service.interface';
 
 @Injectable()
 export class ProductServiceImpl implements ProductService {
-  private readonly logger = new Logger(ProductService.name);
-
   constructor(private readonly httpService: HttpService) {}
 
   async checkStockAvailability(products: CartProductDto[]): Promise<void> {
